a678fa39d2059dc7646a56551a3d93b7946eefb4
SoniaHL <36013810+SoniaHL@users.noreply.github.com>
2021-03-29 18:29:05 -0400

    Added new class to specify seeds and run simulation

---
+++ ManufactorySimulation/src/base/SimulationRunner.java
@@ -0,0 +1,8 @@
+package base;
+
+public class SimulationRunner {
+    public static void main(String[] args) {
+        int[] seeds = {1234,1234,1234,1234,1234,1234};
+        Model.runSimulation(seeds);
+    }
+}
